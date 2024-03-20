c3d0ce594144530038f23af0c932021ff40bb8d9
Bori Sandeva <borislavasandeva@gmail.com>
2024-03-20 09:16:52 +0100

    reset num_epochs

--- nnunet_mednext/training/network_training/nnUNetTrainerV2.py
+++ nnunet_mednext/training/network_training/nnUNetTrainerV2.py
@@ -45,7 +45,7 @@ class nnUNetTrainerV2(nnUNetTrainer):
                  unpack_data=True, deterministic=True, fp16=False, sample_by_frequency=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16, sample_by_frequency)
-        self.max_num_epochs = 50 #30
+        self.max_num_epochs = 1000
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
